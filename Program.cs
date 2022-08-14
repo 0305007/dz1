c11383281e5f9aa72394c4c3ad5fbadc0ff47b23
Мурат <0305007@mail.ru>
2022-08-14 18:41:30 +0300

    домашка

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿//Напишите программу, которая на вход принимает 2 числа и выдает, какое число большее, а какое меньшее
+
+//a = 5; b = 7-> max = 7
+//a = 2b = 10 -> max = 10
+//a =- 9b =- 3 -> max = - 3
+
+// Console.Clear();
+// Console.WriteLine("Программа сравнения");
+// Console.WriteLine("Введите число A");
+// string? numberStringa = Console.ReadLine();
+// int numbera = int.Parse(numberStringa!);
+// Console.Clear();
+
+// Console.WriteLine("Введите число B");
+// string? numberStringb = Console.ReadLine();
+// int numberb = int.Parse(numberStringb!);
+// Console.Clear();
+
+// if (numbera > numberb)
+//     Console.WriteLine(numbera + " max");
+
+// else
+//     Console.WriteLine(numberb + " max");
+
+
+
+
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+//Console.Clear();
+// Console.WriteLine("Программа сравнения");
+// Console.WriteLine("Введите число A");
+// string? numberStringa = Console.ReadLine();
+// int numbera = int.Parse(numberStringa!);
+// Console.Clear();
+
+// Console.WriteLine("Введите число B");
+// string? numberStringb = Console.ReadLine();
+// int numberb = int.Parse(numberStringb!);
+// Console.Clear();
+
+// Console.WriteLine("Введите число C");
+// string? numberStringc = Console.ReadLine();
+// int numberc = int.Parse(numberStringc!);
+// Console.Clear();
+
+// int max = numbera;
+
+// if (max < numberb)
+// {
+//     max = numberb;
+// }
+// if (max  < numberc)
+// {
+//     max = numberc;
+// }
+
+// Console.WriteLine("Большее из трех чисел " + max);
+
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.Clear();
+// Console.WriteLine("Программа определяет число четное или нечетное");
+// Console.WriteLine("Введите число");
+// string? numberStringa = Console.ReadLine();
+// int numbera = int.Parse(numberStringa!);
+// Console.Clear();
+// if (numbera % 2 == 0)
+// {
+//     Console.WriteLine("число четное");
+// }
+
+// else
+// {
+//     Console.WriteLine("число нечетное");
+// }
+
+
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+
+// Console.Clear();
+// Console.WriteLine("Программа определяет число четное или нечетное");
+// Console.WriteLine("Введите число");
+// string? numberStringa = Console.ReadLine();
+// int numbera = int.Parse(numberStringa!);
+// Console.Clear();
+// int i = 2;
+// while (numbera >= i)
+// {
+//   Console.WriteLine (i);
+//    i = i + 2;
+    
+// }
+
+
+
